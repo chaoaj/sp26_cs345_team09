818392a55d989af4b1c98addc9b19d9b7efa2728
orobertjohnson06 <orobertjohnson06@gmail.com>
2026-05-13 09:14:12 -0400

    better relic odds for demon

--- relic.js
+++ relic.js
@@ -1,9 +1,9 @@
 export const RARITY = Object.freeze({
-  COMMON: {label: "Common", color: "#42b3e4", spawnWeight: 20},
-  RARE: {label: "Rare", color: "#df7126", spawnWeight: 10},
+  COMMON: {label: "Common", color: "#42b3e4", spawnWeight: 8},
+  RARE: {label: "Rare", color: "#df7126", spawnWeight: 7},
   EPIC: {label: "Epic", color: "#8a3194", spawnWeight:  5},
-  LEGENDARY: {label: "Legendary", color: "#cf1313", spawnWeight: 3},
-  UNIQUE: {label: "Unique", color: "#fb5ae0", spawnWeight: 2}
+  LEGENDARY: {label: "Legendary", color: "#cf1313", spawnWeight: 4},
+  UNIQUE: {label: "Unique", color: "#fb5ae0", spawnWeight: 3}
 });
 
 
